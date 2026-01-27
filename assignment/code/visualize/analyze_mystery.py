338a3920afd1b97180fd56c17737de10d4290cf7
Masayuki Yamazak <yamamasa27@utexas.edu>
2026-01-26 22:16:15 -0600

    3.1 done

--- assignment/code/visualize/analyze_mystery.py
+++ assignment/code/visualize/analyze_mystery.py
@@ -1,3 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+X = np.load("mysterydata/mysterydata2.npy")
+for i in range(9):
+    # option 1
+    X_1 = np.sqrt(X[:,:,i])
+    plt.imsave("vis2-op1_%d.png" % i, X_1, cmap='plasma')
+
+for i in range(9):
+    # option 2: log correction (log1p is safer to avoid log(0))
+    X_2 = np.log1p(X[:,:,i])
+    plt.imsave("vis2-op2_%d.png" % i, X_2, cmap='plasma')
